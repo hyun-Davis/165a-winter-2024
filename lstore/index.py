fa44ac8d67a366c0add18f7891a33479d043457d
unknown <hanyun@ucdavis.edu>
2024-02-08 13:21:20 -0800

    Major improvements to Index
    - Can now initialize self with data from table.
    - Added functionality to sorted_seeds.
    - general operational improvements for efficiency.
    - TODO: debug implementation, potentially improve some of the jank.

--- lstore/index.py
+++ lstore/index.py
@@ -1,32 +1,46 @@
 from lstore.table import Table, Record
 from lstore.page import Page
+import random
 #import 
 
 """
 Internal node object used for quickly traversing values stored in the index.
 """
 class IndexNode:
-    def __init__(self, in_value, previous_node, next_node, in_rid):
+    def __init__(self, in_value, in_rid):
         self.value = in_value
         self.rid = in_rid
-        self.prev = previous_node
-        self.next = next_node
+        self.next = None
 
+"""
+Storage class for each column for indexing purposes.
+"""
 class IndexStore:
+
     def __init__(self):
-        self.stored_records = [None]
-        self.map_size = 10 # TODO: determine this value more intelligently?
-        self.sorted_seeds = []
+        self.stored_records = {}
+        self.first_node = None
+        self.maximum_value = None
+        self.sorted_seeds = {}
     
     # Finds the largest key that is smaller than the desired value.
     def find_largest_smaller_key(self, desired_value):
+
+        # To handle potential edge cases where the sorted seeds give nothing.
+        desired_node = self.first_node
+
+        # Parse through the sorted seeds list.
         for current_seed in self.sorted_seeds:
             if current_seed != None:
                 continue
             if current_seed.next != None and current_seed.next.value >= desired_value:
                 break
-                
-        desired_node = current_seed
+        
+        if not (current_seed is None):
+            # Find the exact value that is just before where this node should be.
+            desired_node = current_seed
+
+        # Parse for the node exactly before the closest value.
         while desired_node != None and desired_node.next != None:
             if desired_node.next.main_key >= desired_value:
                 break
@@ -35,11 +49,47 @@ class IndexStore:
         return desired_node
     
     def insert_record(self, in_value, in_rid):
-        bucket_placement = in_value % self.map_size
-        self.stored_records[bucket_placement].append(in_rid)
 
-        new_child_node = self.find_largest_smaller_key(in_value)
-        IndexNode(in_value, new_child_node, new_child_node.next, in_rid)
+        if (self.maximum_value is None) or (self.maximum_value < in_value):
+            self.maximum_value = in_value
+
+        new_node = IndexNode(in_value, in_rid)
+        if self.first_node is None:
+            # There is no first node, making this the first.
+            self.first_node = new_node
+        else:
+            if self.first_node.value > in_value:
+                # the new node is smaller than the current smallest.
+                new_node.next = self.first_node
+                self.first_node = new_node
+            else:
+                # insert the record's node into the list of nodes.
+                new_child_node = self.find_largest_smaller_key(in_value)
+                new_node.next = new_child_node.next
+                new_child_node.next = new_node
+
+        # Hash the rid into its storage location in the hash table
+        self.stored_records[in_value].append(new_node)
+
+        # Update the sorted seeds.
+        """
+        Randomly step through values up to max_node divided by some value.
+        """
+        max_node = self.find_largest_smaller_key(self.maximum_value + 1)
+        # TODO: more intelligent addition of values.
+        i = self.first.node.value + random.randint(1, max_node.value / 5)
+
+        self.sorted_seeds.clear()
+
+        # Sequentially add to sorted_seeds until the maximum node value.
+        while i < max_node.value:
+            
+            # As a set, sorted_seeds should reject duplicates.
+            self.sorted_seeds.add(self.find_largest_smaller_key(i))
+        
+        self.sorted_seeds.sort()
+
+
 
         
 
@@ -50,13 +100,15 @@ A data strucutre holding indices for various columns of a table. Key column shou
 class Index:
 
     def __init__(self, table):
+        # store the table.
+        self.table_ref = table
+
         # One index for each table. All our empty initially.
         self.indices = [None] *  table.num_columns
 
+        # initialize each column in the index storage.
         for i in range(table.num_columns):
             self.create_index(i)
-        
-        #TODO: insert individual records into the index.
                 
         pass
 
@@ -68,14 +120,7 @@ class Index:
     def locate(self, column, value):
         
         # All data that maps to value should hash to indices[column]
-        locations = self.indices[column].stored_records[value][:]
-
-        # Remove unnecessary data points.
-        for rec in locations:
-            if rec.value != value:
-                locations.remove(rec)
-
-        return locations
+        return self.indices[column].stored_records[value][:]
         
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
@@ -101,7 +146,16 @@ class Index:
     """
 
     def create_index(self, column_number):
-        self.indices[column_number] = IndexStore()
+        created_store = IndexStore()
+        self.indices[column_number] = created_store
+
+        # catlogue the indices of each individual record.
+        for i in range(self.table.farthest['pi'] + 1):
+            page = self.table.page_directory[i]
+            for j in range(self.table.farthest['slot_index'] + 1):
+                record = page.records[j]
+
+                created_store.insert_record(record.columns[column_number], record.rid)
         pass
 
     """
