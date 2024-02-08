acd33818b94d9c8f4b031b84d3cef51ba7effd27
unknown <hanyun@ucdavis.edu>
2024-02-08 04:46:37 -0800

    Improved Indexing
    Added higher-level functionality to Index class. Still unsure of how to extract records from table....

--- lstore/index.py
+++ lstore/index.py
@@ -1,4 +1,47 @@
-import lstore.table
+from lstore.table import Table, Record
+from lstore.page import Page
+#import 
+
+"""
+Internal node object used for quickly traversing values stored in the index.
+"""
+class IndexNode:
+    def __init__(self, in_value, previous_node, next_node, in_rid):
+        self.value = in_value
+        self.rid = in_rid
+        self.prev = previous_node
+        self.next = next_node
+
+class IndexStore:
+    def __init__(self):
+        self.stored_records = [None]
+        self.map_size = 10 # TODO: determine this value more intelligently?
+        self.sorted_seeds = []
+    
+    # Finds the largest key that is smaller than the desired value.
+    def find_largest_smaller_key(self, desired_value):
+        for current_seed in self.sorted_seeds:
+            if current_seed != None:
+                continue
+            if current_seed.next != None and current_seed.next.value >= desired_value:
+                break
+                
+        desired_node = current_seed
+        while desired_node != None and desired_node.next != None:
+            if desired_node.next.main_key >= desired_value:
+                break
+            desired_node = desired_node.next
+        
+        return desired_node
+    
+    def insert_record(self, in_value, in_rid):
+        bucket_placement = in_value % self.map_size
+        self.stored_records[bucket_placement].append(in_rid)
+
+        new_child_node = self.find_largest_smaller_key(in_value)
+        IndexNode(in_value, new_child_node, new_child_node.next, in_rid)
+
+        
 
 """
 A data strucutre holding indices for various columns of a table. Key column should be indexed by default, other columns can be indexed through this object. Indices are usually B-Trees, but other data structures can be used as well.
@@ -10,9 +53,11 @@ class Index:
         # One index for each table. All our empty initially.
         self.indices = [None] *  table.num_columns
 
-        # Functionality for storing the sorted seeds with which to quickly index
-        self.sorted_seeds = []
+        for i in range(table.num_columns):
+            self.create_index(i)
+
         pass
+
 
     """
     # returns the location of all records with the given value on column "column"
@@ -20,29 +65,13 @@ class Index:
 
     def locate(self, column, value):
         
-        locations = []
+        # All data that maps to value should hash to indices[column]
+        locations = self.indices[column].stored_records[value][:]
 
-        column_data = self.indices[column]
-
-        for indexedRecord in column_data:
-            if indexedRecord.key == value:
-                locations.append(indexedRecord.rid)
-            
-
-        '''target_key = self.sorted_seeds[0]
-        for indexed_key in self.sorted_seeds:
-            if indexed_key > value:
-                break
-            else:
-                target_key = indexed_key'''    
-
-        """
-        while (next <= value && next exists):
-          if target_key.value == value:
-            
-          else:
-            taget_key = target_key.next
-        """
+        # Remove unnecessary data points.
+        for rec in locations:
+            if rec.value != value:
+                locations.remove(rec)
 
         return locations
         
@@ -54,10 +83,15 @@ class Index:
         
         foundRIDs = []
 
-        for iRecord in self.indices[column].records:
-            if iRecord.value <= begin and iRecord.value >= end:
-                foundRIDs.append(iRecord.rid)
-            
+        col_data = self.indices[column]
+        desired_node = col_data.find_largest_smaller_key(begin)
+        if not(desired_node is None):
+
+          # the first node found is actually the one smaller than begin.
+          desired_node = desired_node.next  
+          while desired_node != None and desired_node.value <= end:
+              foundRIDs.append(desired_node.rid)
+
         return foundRIDs
 
     """
@@ -65,7 +99,7 @@ class Index:
     """
 
     def create_index(self, column_number):
-        # self.indices[column_number].append()
+        self.indices[column_number] = IndexStore()
         pass
 
     """
